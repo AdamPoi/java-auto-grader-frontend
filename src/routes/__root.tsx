45df96435f5b0b2edd84c2c28a6252eec7bdf18b
AdamPoi <isadamrr@gmail.com>
2025-05-19 11:48:13 +0700

    feat: add initial shadcn component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,53 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { createRootRoute, Link, Outlet, useRouter } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { useAuthStore } from '@/store/auth'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+  component: () => {
+    const router = useRouter()
+    const token = useAuthStore((state) => state.token)
+    const logout = useAuthStore((state) => state.logout)
+
+    return (
+      <>
+        <div className="p-2 flex gap-2">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{' '}
+          <Link to="/about" className="[&.active]:font-bold">
+            About
+          </Link>
+          {!token && (
+            <>
+              <Link to="/login" className="[&.active]:font-bold ml-2">
+                Login
+              </Link>
+              <Link to="/register" className="[&.active]:font-bold ml-2">
+                Register
+              </Link>
+            </>
+          )}
+          {token && (
+            <>
+              <Link to="/dashboard" className="[&.active]:font-bold ml-2">
+                Dashboard
+              </Link>
+              <button
+                onClick={() => {
+                  logout()
+                  router.navigate({ to: '/login' })
+                }}
+                className="ml-2 text-red-500"
+              >
+                Logout
+              </button>
+            </>
+          )}
+        </div>
+        <hr />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </>
+    )
+  },
 })
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
-import '../App.css'
 
 export const Route = createFileRoute('/')({
-  component: App,
+  component: Index,
 })
 
-function App() {
+function Index() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </header>
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
     </div>
   )
 }
