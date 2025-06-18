1a0f60523f2092e41713a6fdf544ac51132b512e
nyaw <isadamrr@gmail.com>
2025-06-18 10:39:23 +0700

    fix: env example variable name

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,35 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(),tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+     
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@tabler/icons-react":
+        "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
+
   test: {
     globals: true,
     environment: "jsdom",
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
-
-    },
-  }
 });
